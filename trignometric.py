f73a6ecb205e438f63684768400fd778b11c92ce
aniesh <aniesh@sanctumnetworks.com>
2015-08-07 15:54:55 +0530

    algorithm for the challenge https://www.hackerrank.com/contests/codegolf-practice-match-1/challenges/trignometric-ratios/submissions/code/3558533 which ask for doing this in less than 400 characters, I am presently getting 40% of it in ans

--- trignometric.py
+++ trignometric.py
@@ -1,7 +1,18 @@
-# b = [(lambda b: (lambda a, b: a(a, b))(lambda a, b: b*a(a, b-1) if b > 0 else 1,b))(num) for num in range(1,10)]
-fact=[1]*9
+f=[1]*10
+p="{0:.3f}".format
 for b in range(1,10):
-	fact[b] = fact[b-1]*b
-
-for i in range(int(raw_input())):
-	
+	f[b] = f[b-1]*b
+for i in range(input()):
+	x=float(input())
+	s=c=v=0
+	y=m=1
+	while(v<10):
+		o=(m)*y/f[v]
+		if(v%2==0):
+			c+=o
+		else:
+			s+=o
+			m*=-1
+		y*=x
+		v+=1
+	print p(s),'\n',p(c)
